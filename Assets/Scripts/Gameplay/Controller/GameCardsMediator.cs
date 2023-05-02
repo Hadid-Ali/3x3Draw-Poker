a469578c32126f8c86a9e20c97b5830456cf7a94
Hadid-Ali <blunty.hadid@gmail.com>
2023-05-02 20:35:41 +0500

    Add Restart Button

--- Assets/Scripts/Gameplay/Controller/GameCardsMediator.cs
+++ Assets/Scripts/Gameplay/Controller/GameCardsMediator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameCardsMediator : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class GameCardsMediator : MonoBehaviour
     {
         get; 
         private set;
+    }
+    
+    //TODO: Refactor
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     private void OnEnable()
