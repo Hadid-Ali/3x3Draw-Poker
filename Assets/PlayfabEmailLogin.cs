eb596a7edda9d48b8e4ab4664601f9e57113b278
Ali hamza <gtassan533083@gmail.com>
2024-06-24 14:13:42 +0500

    new changes

--- Assets/PlayfabEmailLogin.cs
+++ Assets/PlayfabEmailLogin.cs
@@ -55,7 +55,7 @@ public class PlayfabEmailLogin : MonoBehaviour
         //PlayFabClientAPI.LinkGameCenterAccount(LinkGameCenter,OnLinkSucess,OnloginFailed);
         var EmailLoginRequst = new LoginWithEmailAddressRequest
         {
-          
+
             Email = User_email,
             Password = User_pass
         };
