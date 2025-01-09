e16ba77ee7c6eb2c9b01e6979ade67d9cd9e10d3
Ibrahim <97936461+IbrahimBu11@users.noreply.github.com>
2025-01-09 14:17:55 +0500

    Poker Finalized again

--- Assets/Scripts/Gameplay/Controller/RandomizeObjectController.cs
+++ Assets/Scripts/Gameplay/Controller/RandomizeObjectController.cs
@@ -1,27 +1,63 @@
 using System.Collections.Generic;
+using ExitGames.Client.Photon;
 using Photon.Pun;
 using UnityEngine;
 
-public class RandomizeObjectController : MonoBehaviour
+public class RandomizeObjectController : MonoBehaviourPunCallbacks
 {
     [SerializeField] private List<GameObject> m_RandomObjects;
     [SerializeField] private PhotonView view;
+
+    private const string RandomObjectKey = "RandomObjectIndex";
 
     private void Start()
     {
         if (PhotonNetwork.OfflineMode)
         {
-            view.RPC(nameof(OnMasterInitialized),RpcTarget.AllBuffered, Random.Range(0,m_RandomObjects.Count));
+            int randomIndex = Random.Range(0, m_RandomObjects.Count);
+            OnMasterInitialized(randomIndex);
             return;
         }
+            
         
-        if(!PhotonNetwork.IsMasterClient)
-            view.RPC(nameof(OnMasterInitialized),RpcTarget.AllBuffered, Random.Range(0,m_RandomObjects.Count));
+        if (PhotonNetwork.IsMasterClient)
+        {
+            // Generate a random index and store it in room properties
+            int randomIndex = Random.Range(0, m_RandomObjects.Count);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(new Hashtable { { RandomObjectKey, randomIndex } });
+
+            // Initialize the object for the master client
+            OnMasterInitialized(randomIndex);
+        }
+        else 
+        {
+            int randomIndexx = (int)PhotonNetwork.CurrentRoom.CustomProperties[RandomObjectKey];
+            OnMasterInitialized(randomIndexx);
+        }
+        
     }
-    
-    [PunRPC]
+
+    public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
+    {
+        // Check if the RandomObjectIndex property was updated
+        if (propertiesThatChanged.ContainsKey(RandomObjectKey))
+        {
+            int randomIndex = (int)PhotonNetwork.CurrentRoom.CustomProperties[RandomObjectKey];
+            OnMasterInitialized(randomIndex);
+        }
+    }
+
     private void OnMasterInitialized(int val)
     {
+        // Deactivate all objects first
+        foreach (var obj in m_RandomObjects)
+        {
+            obj.SetActive(false);
+        }
+
+        // Activate the selected object
         m_RandomObjects[val].SetActive(true);
+        Debug.Log($"Random Object Activated: {val}");
     }
 }
+
